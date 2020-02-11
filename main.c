34c55dd78d3974e08332e91eb527a7d9b81a7010
Fyquix <60101716+Fyquix@users.noreply.github.com>
2020-02-11 10:38:09 +0100

    Add files via upload
    main.c and timelib.h

--- main.c
+++ main.c
@@ -1,24 +1,30 @@
-/**
- *
- *
- *
- **/
-
+/*
+    Programm: TagDesJahres
+    Author: Florian Pachulleck
+    Datum: 28.1.2020
+*/
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- *
- *
- **/
-int day_of_the_year(int day, int month, int year)
-{
-
-    return 0;
-}
+#include "timelib.h"
 
 int main()
 {
-    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
+    int tag,monat,jahr = 0;
+
+    do
+    {
+      printf("Tag:");
+        scanf("%d", &tag);
+        printf("Monat:");
+        scanf("%d", &monat);
+        printf("Jahr:");
+        scanf("%d", &jahr);
+    }
+    while(!exists_date(tag,monat,jahr));
+
+    int TagImJahr = day_of_the_year(tag,monat,jahr);
+    printf("\nTag im Jahr: %d \n", TagImJahr);
+
+
     return 0;
 }
